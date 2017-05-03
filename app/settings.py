78a44794ddd8b2a79b058cc89b4de573f20f3933
Sebastian Braun <sebastian@elmnt.de>
2017-05-03 15:13:39 +0200

    added memcached

--- app/settings.py
+++ app/settings.py
@@ -332,7 +332,7 @@ if 'MEMCACHED_PORT_11211_TCP_ADDR' in os.environ:  # pragma: no cover
     SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
     CACHES = {
        'default': {
-           'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+           'BACKEND': 'django.core.cache.backends.memcached.PyLibMCCache',
            'KEY_PREFIX': PROJECT_NAME,
            'LOCATION': '%s:%s' % (
                 os.environ.get('MEMCACHED_PORT_11211_TCP_ADDR'),
